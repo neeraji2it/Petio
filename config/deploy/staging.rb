9c41a42e5c2df0345ccccbd10838a8af8e9cfffc
rails <rails@rails.com>
2015-02-27 18:35:53 +0530

    directly upload to main server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{petio.rensofttechnosys.com}
-role :web, %w{petio.rensofttechnosys.com}
-role :db,  %w{petio.rensofttechnosys.com}
+role :app, %w{root@5.9.111.55}
+role :web, %w{root@5.9.111.55}
+role :db,  %w{root@5.9.111.55}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{petio.rensofttechnosys.com}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '5.9.111.55', user: 'petio', roles: %w{web app}, my_property: :my_value 
+server '5.9.111.55', user: 'root', port: 2222, roles: %w{web app}, my_property: :my_value 
 
 
 set :repo_url, 'git@github.com:neeraji2it/Petio.git' 
